1149a82064949d5ea3dfc2b64ef23b426f7bbb5f
QuangTuan165 <lequangtuan@gmail.com>
2021-10-17 22:55:45 +0700

    chat real time

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const usersRouter = require('./users');
 const authRouter = require('./auth');
 const postsRouter = require('./posts');
+const conversationRouter = require('./conversations');
+const messageRouter = require('./messages');
 
 function route(app) {
     app.use('/api/users', usersRouter);
     app.use('/api/auth', authRouter);
     app.use('/api/posts', postsRouter);
+    app.use('/api/conversations', conversationRouter);
+    app.use('/api/messages', messageRouter);
 }
 
 module.exports = route;
